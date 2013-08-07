43040e8fa26debb25a8f0fd4b26e175eb712d629
ilyashulgin <ishulgin8@gmail.com>
2013-08-07 21:14:42 +0400

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LifeConnection::Application.config.secret_token = 'ec24e3d4dc22b8668e3635806ee43d587c95dd3f7341b3809a9524f6250a7678daa5442ae3d71745e806369e71624120d16bd06d23a3bc1c909ec2fe9b097696'
